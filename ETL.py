53a8ca4d1876c3d559120886bc5d12290dc51d63
breno <brenoouro@gmail.com>
2026-09-17 21:12:00 -0300

    função de load criada e otimizada, criação de arquivos json em pastas específicas e chamadas totais para a API atrás dos dados

--- ETL.py
+++ ETL.py
@@ -1,6 +1,7 @@
 import requests
-# import json
+import json
 
+# função de extração dos dados da API
 def extract_data(endpoint):
     response = requests.get(endpoint)
     if response.status_code == 200:
@@ -9,9 +10,43 @@ def extract_data(endpoint):
         print(f"Erro na extração dos dados da API: {response.status_code}")
         return None
 
-endpoint_users = 'https://dummyjson.com/users/1'
-endpoint_products = 'https://dummyjson.com/products/1'
+# função de carregamento dos dados da API para um arquivo .json em uma pasta, no caso a pasta 'users'
+def load_data(data, path):
+    id = data["id"]
 
-data_users = extract_data(endpoint_users)
+    # salva o nome do arquivo .json na pasta como o id do usuário .json
+    with open(f"{path}/{id}.json", "w") as file:
+        json.dump(data, file)
 
-print(data_users)
+
+# endpoint_users = "https://dummyjson.com/users/"
+# endpoint_products = "https://dummyjson.com/products/"
+
+# # extrai até o final todos os dados presentes na API através de um loop infinito que quebra ao terminar os usuários na API
+# count = 1
+# while True:
+#     data_users = extract_data(endpoint_users + str(count))
+#     if data_users:
+#         load_data(data_users, "users")
+#     else:
+#         print(f"Erro na extração dos dados da API: {data_users}")
+#         break
+#     count += 1
+
+def loop_load_data(endpoint):
+    endpoint = 'https://dummyjson.com/' + endpoint
+
+    count = 1
+    while True:
+        data = extract_data(endpoint + str(count))
+        if data:
+            load_data(data, endpoint)
+        else:
+            print(f"Erro na extração dos dados da API: {data}")
+            break
+        count += 1
+
+endpoints = ["users", "products"]
+
+for endpoint in endpoints:
+    loop_load_data(endpoint)
